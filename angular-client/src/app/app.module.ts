94b30b2fb4c9a2389f52f25f82f71541b17ff72c
djarbo <djgrove@calpoly.edu>
2018-08-31 17:59:07 -0700

    UI attempt #2, got navbar and sass working better

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
