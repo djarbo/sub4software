6772d86ee1133277b5c96c553062ec51177e8789
djarbo <djgrove@calpoly.edu>
2019-01-29 22:20:56 -0800

    restructured project according to style guide

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+import { TasksComponent } from '@app/tasks/tasks.component';
 import { CasesComponent } from './cases/cases.component';
-import { ReportsComponent } from './reports/reports.component';
-import { HomeComponent } from './home/home.component';
-import { SettingsComponent } from './settings/settings.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReportsComponent } from '@app/reports/reports.component';
+import { HomeComponent } from '@app/home/home.component';
+import { SettingsComponent } from 'app/settings/settings.component';
+import { CalendarComponent } from '@app/calendar/calendar.component';
+import { PageNotFoundComponent } from '@app/shared/page-not-found/page-not-found.component';
+import { DashboardComponent } from '@app/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { CasesComponent } from './cases/cases.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ReportsComponent } from './reports/reports.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CalendarComponent } from '@app/calendar/calendar.component';
+import { CasesComponent } from '@app/cases/cases.component';
+import { DashboardComponent } from '@app/dashboard/dashboard.component';
+import { HomeComponent } from '@app/home/home.component';
+import { NavbarComponent } from '@app/shared/navbar/navbar.component';
+import { ReportsComponent } from '@app/reports/reports.component';
+import { SettingsComponent } from '@app/settings/settings.component';
+import { SidebarComponent } from '@app/shared/sidebar/sidebar.component';
+import { TasksComponent } from '@app/tasks/tasks.component';
+import { PageNotFoundComponent } from '@app/shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
